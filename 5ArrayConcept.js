8290deed8acca3d3234c26efc59e357ef23c938e
madhu dharani <cmdharani2014@gmail.com>
2024-05-09 22:45:43 +0530

    array reference type and Arrow function

--- 5ArrayConcept.js
+++ 5ArrayConcept.js
@@ -20,7 +20,7 @@ numbers.splice(2,0,'a','r');
 console.log('numbers ',numbers);
 
 
-//finding elements
+//finding elements primitives
 
 const numbersOne=[1,2,3,4,1,5];
 
@@ -32,3 +32,31 @@ console.log(numbersOne.lastIndexOf(1));
 
 console.log(numbersOne.indexOf(1) !== 1);
 console.log(numbersOne.includes(1));
+
+
+
+//finding elements Reference type
+
+console.log('\nFinding elements Reference type')
+
+const movies=[
+    {id:1,name:'a'},
+    {id:2,name:'b'},
+];
+
+const movie=movies.find(function(movie){
+    return movie.name==='a'
+});
+
+console.log(movie);
+
+const movie1=movies.find(function(movie){
+    return movie.name==='xyz'
+});
+
+console.log(movie1);
+
+console.log('Arrow function');
+
+const movie2=movies.find(movie=> movie.name ==='a');
+console.log(movie2);
